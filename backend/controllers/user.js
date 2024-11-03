a2c132c3bdec00a974ab086ada991e264e7171ed
rodicailciuc <rodicailciuc@yahoo.com>
2024-11-03 20:59:54 +0100

    add recipe

--- backend/controllers/user.js
+++
@@ -1,9 +0,0 @@
-import User from '../models/user.js';
-
-const userControllers = {
-    register: async (req, res) => {},
-    login: async (req, res) => {},
-    logout: async (req, res) => {}
-};
-
-export default userControllers;
--- backend/utils/hashPassword.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const hashPassword = (password) => {
-    const salt = bcrypt.genSaltSync(10);
-    return bcrypt.hashSync(password, salt);
-};
-
-export default hashPassword;
